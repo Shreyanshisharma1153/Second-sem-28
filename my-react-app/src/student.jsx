851371d3f3974fff4496e37a6d2b8f9872075b72
Shreyanshi Sharma <shreyanshis1619@gmail>
2026-05-26 14:26:13 +0530

    JS

---
+++ my-react-app/src/student.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Student() {
+  return (
+    <div>
+        <h1>details</h1>
+        <h2> STUDENT INFORMATION</h2>
+
+        <audio controls>
+        <source src="" type="audio/mp3/"></source>
+      </audio>
+      <video controls>
+        <source src="" type="video/mp4/"></source>
+      </video>
+      <iframe src="https://www.youtube.com/embed/FpM9m12qjZE"></iframe>
+      <img src="https://images.pexels.com/photos/35383830/pexels-photo-35383830.jpeg" width="500" height="500"></img><br></br>
+      <button>Click ME</button>
+    <h2>Hello ABESEC</h2>
+    <p>Hello Jiya</p>
+      
+    </div>
+  )
+}
+
+export default Student
